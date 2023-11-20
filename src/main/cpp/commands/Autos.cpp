28f510aa778c4c9a1d8eb60021130ad610d345ad
5690 <5690Programmers@gmail.com>
2023-11-20 16:43:55 -0600

    nolan

--- src/main/cpp/commands/Autos.cpp
+++ src/main/cpp/commands/Autos.cpp
@@ -1,23 +1,17 @@
-// #include "commands/Autos.h"
-// #include <pathplanner/lib/auto/SwerveAutoBuilder.h>
-// #include <pathplanner/lib/PathPlanner.h>
+#include "commands/Autos.h"
+#include <frc/geometry/Pose2d.h>
+#include <pathplanner/lib/path/PathPlannerPath.h>
+#include <pathplanner/lib/auto/AutoBuilder.h>
 
-// using namespace pathplanner;
-// using namespace autos;
+using namespace pathplanner;
+using namespace autos;
 
-// std::vector<PathPlannerTrajectory> pathGroup = PathPlanner::loadPathGroup("FullAuto", {PathConstraints(4_mps, 3_mps_sq)});
+std::unordered_map<std::string, std::shared_ptr<frc2::Command>> eventMap;
 
-// std::unordered_map<std::string, std::shared_ptr<frc2::Command>> eventMap;
+AutoBuilder::configureHolonomic(
+    [this]() { return m_drive.GetPose(); }, // Function to supply current robot pose
+    [this](frc::Pose2d initPose) { m_drive.ResetOdometry(initPose); }, // Function used to reset odometry at the beginning of auto
+    [this]() {return m_drive.getSpee}
+    );
 
-// SwerveAutoBuilder autoBuilder(
-//     []() { return m_drive.GetPose(); }, // Function to supply current robot pose
-//     [](auto initPose) { m_drive.ResetOdometry(initPose); }, // Function used to reset odometry at the beginning of auto
-//     PIDConstants(5.0, 0.0, 0.0), // PID constants to correct for translation error (used to create the X and Y PID controllers)
-//     PIDConstants(0.5, 0.0, 0.0), // PID constants to correct for rotation error (used to create the rotation controller)
-//     [](auto speeds) { m_drive.Drive(speeds); }, // Output function that accepts field relative ChassisSpeeds
-//     eventMap, // Our event map
-//     { &m_drive }, // Drive requirements, usually just a single drive subsystem
-//     true // Should the path be automatically mirrored depending on alliance color. Optional, defaults to true
-// );
-
-// frc2::CommandPtr fullAuto = autoBuilder.fullAuto(pathGroup);
+frc2::CommandPtr fullAuto = autoBuilder.fullAuto(pathGroup);
