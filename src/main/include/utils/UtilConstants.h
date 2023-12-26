16ed33a2e4a79d5459887fcf1a9237a177c38a16
5690 <5690Programmers@gmail.com>
2023-12-25 22:13:34 -0600

    wrist (#2)
    * skeleton changes from thursday
    
    * cleanup wrist, get building in 2024
    
    * Misc fixes
    
    ---------
    
    Co-authored-by: having11 <gatoninja236@gmail.com>

--- src/main/include/utils/UtilConstants.h
+++ src/main/include/utils/UtilConstants.h
@@ -4,4 +4,4 @@ namespace Logging {
 enum class Level { VERBOSE = 0, INFO, WARNING, ERROR };
 // Don't log at levels below this one
 constexpr auto kMinLogLevel = Level::VERBOSE;
-} // namespace Logging
+}  // namespace Logging
