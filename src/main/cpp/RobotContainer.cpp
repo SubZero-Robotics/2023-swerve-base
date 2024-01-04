b4007e3efdcffbeef6f1beb39078606a6b9db15d
nobody5050 <levibelland@gmail.com>
2024-01-04 02:23:23 -0600

    use sendable chooser for auto

--- src/main/cpp/RobotContainer.cpp
+++ src/main/cpp/RobotContainer.cpp
@@ -22,6 +22,7 @@
 
 #include "Constants.h"
 #include "subsystems/DriveSubsystem.h"
+#include "utils/ShuffleboardLogger.h"
 #include "commands/Funni.h"
 #include "commands/LEDToggleCommand.h"
 #include "commands/RotateWristCommand.h"
@@ -56,6 +57,7 @@ RobotContainer::RobotContainer() {
       {&m_drive}));
 
     m_chooser.SetDefaultOption("Leave Community", pathplanner::PathPlannerAuto("Leave COM").ToPtr().Unwrap().get());
+    shuffleboardLogger.logVerbose("Auto Modes", &m_chooser);
 }
 
 void RobotContainer::ConfigureButtonBindings() {
@@ -79,50 +81,5 @@ void RobotContainer::ConfigureButtonBindings() {
 }
 
 frc2::Command* RobotContainer::GetAutonomousCommand() {
-  // Set up config for trajectory
-  frc::TrajectoryConfig config(AutoConstants::kMaxSpeed,
-                               AutoConstants::kMaxAcceleration);
-  // Add kinematics to ensure max speed is actually obeyed
-  config.SetKinematics(m_drive.kDriveKinematics);
-
-  // An example trajectory to follow.  All units in meters.
-  auto exampleTrajectory = frc::TrajectoryGenerator::GenerateTrajectory(
-      // Start at the origin facing the +X direction
-      frc::Pose2d{0_m, 0_m, 0_deg},
-      // Make these be 0m so it drives a straight line
-      {frc::Translation2d{0_m, 0_m}, frc::Translation2d{0_m, 0_m}},
-      // End 3 meters straight ahead of where we started, facing forward
-      frc::Pose2d{3_m, 0_m, 0_deg},
-      // Pass the config
-      config);
-
-  frc::ProfiledPIDController<units::radians> thetaController{
-      AutoConstants::kPThetaController, 0, 0,
-      AutoConstants::kThetaControllerConstraints};
-
-  thetaController.EnableContinuousInput(units::radian_t{-std::numbers::pi},
-                                        units::radian_t{std::numbers::pi});
-
-  frc2::SwerveControllerCommand<4> swerveControllerCommand(
-      exampleTrajectory, [this]() { return m_drive.GetPose(); },
-
-      m_drive.kDriveKinematics,
-
-      frc::PIDController{AutoConstants::kPXController, 0, 0},
-      frc::PIDController{AutoConstants::kPYController, 0, 0}, thetaController,
-
-      [this](auto moduleStates) { m_drive.SetModuleStates(moduleStates); },
-
-      {&m_drive});
-
-  // Reset odometry to the starting pose of the trajectory.
-  m_drive.ResetOdometry(exampleTrajectory.InitialPose());
-
-  // no auto
-  return new frc2::SequentialCommandGroup(
-      frc2::InstantCommand(
-          [this]() {
-            m_drive.Drive(0_mps, 0_mps, 0_rad_per_s, false, false, kLoopTime);
-          },
-          {}));
+    return m_chooser.GetSelected();
 }
